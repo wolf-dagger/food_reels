6f61e18c9b80d425d4d38f1e3a00e9e0eedc5ec6
wolf-dagger <73078569+wolf-dagger@users.noreply.github.com>
2026-08-31 21:09:41 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,5 @@
+// start server
+
+const app = require("./src/app");
+
+app.listen(3000, () => console.log("Listening on port 3000"));
---
+++ backend/src/app.js
@@ -0,0 +1,10 @@
+// create server
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+module.exports = app;
